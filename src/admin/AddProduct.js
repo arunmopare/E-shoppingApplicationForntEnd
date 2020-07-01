966f9a3f2c74d0ef97344f97f3d2fc72ca6769ee
arunmopare <arunmopare546@gmail.com>
2020-07-01 17:31:10 +0530

    Done with home page UI

--- src/admin/AddProduct.js
+++ src/admin/AddProduct.js
@@ -76,7 +76,7 @@ const AddProduct = () => {
   };
 
   const handleChange = (name) => (event) => {
-    const value = name === "photo" ? event.target.filesd    [0] : event.target.value;
+    const value = name === "photo" ? event.target.files[0] : event.target.value;
     formData.set(name, value);
     setvalues({ ...values, [name]: value });
   };
--- src/core/Home.js
+++ src/core/Home.js
@@ -1,23 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../styles.css";
 import { API } from "../backend";
 import Base from "./Base";
 import Card from "./card";
+import { getProducts } from "./helper/coreapicalls";
 
 export default function Home() {
+  const [products, setproducts] = useState([]);
+  const [error, seterror] = useState(false);
+
+  const loadAllProducts = () => {
+    getProducts().then((data) => {
+      if (data.error) {
+        seterror(data.error);
+      } else {
+        setproducts(data);
+      }
+    });
+  };
+  useEffect(() => {
+    loadAllProducts();
+  }, []);
   return (
     <Base title='Home Page' description='Welcome to T-shirt Store'>
-      <h1 className='text-white'>Hello FrontEnd </h1>
+        <h1 className='text-white text-center mb-4'>All Of TS</h1>
       <div className='row text-center'>
-        <div className='col-4'>
-          <Card></Card>
-        </div>
-        <div className='col-4'>
-          <Card></Card>
-        </div>
-        <div className='col-4'>
-          <Card></Card>
-        </div>
+        {products.map((prduct, index) => {
+          return (
+            <div key={index} className='col-4 mb-4'>
+              <Card  />
+            </div>
+          );
+        })}
       </div>
     </Base>
   );
