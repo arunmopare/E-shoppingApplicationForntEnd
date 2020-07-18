4c01b70e5be2a0824e3dd77f7a963ab95282cb15
arunmopare <arunmopare546@gmail.com>
2020-07-18 17:49:35 +0530

    Taking backup as windows is crashing

--- src/core/StripeCheckout.js
+++ src/core/StripeCheckout.js
@@ -3,6 +3,9 @@ import { isAuthenticated } from "../auth/helper";
 import { cartEmpty, loadCart } from "./helper/cartHelper";
 import { Link } from "react-router-dom";
 import { getDefaultNormalizer } from "@testing-library/react";
+import StripeCheckoutButton from "react-stripe-checkout";
+import { API } from "../backend";
+import { createOrder } from "./helper/orderHelper";
 
 const StripeCheckout = ({
   products,
@@ -26,17 +29,48 @@ const StripeCheckout = ({
     });
     return amount;
   };
-  const showStripeButton =() =>{
-      return isAuthenticated() ? (
-          <button className="btn btn-success">Pay With Stripe</button>
-      ) :(<Link to="/signin">
-          <button className="btn btn-warning">Signin</button>
-      </Link>)
-  }
+  const makePayment = (token) => {
+    const body = {
+      token,
+      products,
+    };
+    const headers = {
+      "Content-Type": "application/json",
+    };
+    return fetch(`${API}/stripepayment`, {
+      method: "POST",
+      headers,
+      body: JSON.stringify(body),
+    })
+      .then((responce) => {
+        console.log(responce);
+      })
+      .catch((error) => console.log(error));
+  };
+  const showStripeButton = () => {
+    return isAuthenticated() ? (
+      <StripeCheckoutButton
+        stripeKey='pk_test_51H15sUHrifiYqPQD6SoeekAmntJt9csDC2aQYARCOiQS6xA1SK8Gb1TEQJgORhG6czvmhqht6Ls7Vf4xb1c5Am0W00n9y0sZ9U'
+        token={makePayment}
+        amount={getFinalPrice() * 100}
+        name='Buy T-shirts'
+        shippingAddress
+        billingAddress
+      >
+        <button className='btn btn-success'>Pay With Stripe</button>
+      </StripeCheckoutButton>
+    ) : (
+      <Link to='/signin'>
+        <button className='btn btn-warning'>Signin</button>
+      </Link>
+    );
+  };
 
   return (
     <div>
-      <h3 className='text-white'>Stripe Checkout Proce is INR. {getFinalPrice()}</h3>
+      <h3 className='text-white'>
+        Stripe Checkout Proce is INR. {getFinalPrice()}
+      </h3>
       {showStripeButton()}
     </div>
   );
